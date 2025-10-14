e4dcc8af818b762f42d775e3ce748a9faa22c7ad
Ariff-dev <ariff.dev24@gmail.com>
2025-10-13 23:53:43 -0600

    feat:Navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,10 +1,6 @@
 import type { AppContentNav } from "./ui.interface"
 
-
-
 export const Navbar = () => {
-    
-
     const appcontent:AppContentNav[] = [
         { name: 'Hero', tag: '#hero', },
         { name: 'About', tag: '#about' },
@@ -12,14 +8,13 @@ export const Navbar = () => {
         { name: 'Experience', tag: '#exp' },
         { name: 'Contact', tag: '#contact' },
     ]
-
   return (
     <nav className="flex gap-2">
         {appcontent.map(( {tag,name},idx ) => {
             return (
                 <p>
-                    <a href={tag}>
-                        {name}<span className="">{idx < appcontent.length - 1 && ' >'}</span>
+                    <a href={tag} className="text-(--secondary)">
+                        {name}<span className="text-(--primary)">{idx < appcontent.length - 1 && ' >'}</span>
                     </a>
                 </p>
             )
