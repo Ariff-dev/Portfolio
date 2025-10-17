ef21d6823ebc4c4b0276ca05fad7727a616c1e3e
Ariff-dev <ariff.dev24@gmail.com>
2025-10-16 21:10:49 -0600

    feat: About Section

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,9 +1,11 @@
+import { About } from "./about/About"
 import { Hero } from "./hero/Hero"
 
 export const Landing = () => {
   return (
     <>
-        <Hero/>
+        <Hero />
+        <About />
     </>
   )
 }
---
+++ src/components/landing/about/About.tsx
@@ -0,0 +1,17 @@
+
+export const About = () => {
+  return (
+    <section id="#about" className="h-min-screen flex flex-col gap-4">
+        <h2 className="text-(--primary) text-xl">Hello There!</h2>
+        <p>
+            Hi there! I’m a product-focused Software Developer and a final-year Software Engineering student passionate about building technology that solves real problems. My journey into tech began at 15, when I learned Java to build a Minecraft server for my friends — an experience that sparked my curiosity for purposeful, hands-on problem-solving.
+        </p>
+        <p>
+            Over the years, I’ve developed a strong foundation in software architecture, databases, and cloud computing through my studies at Hybridge University. I started out in front-end development with React and Angular, and later expanded into back-end and data engineering, where I found my real passion.
+        </p>
+        <p>
+            Today, I work as a Software Developer & Analyst in the FinTech sector, focusing on PHP-based back-end development, process automation, and data-driven tools that enhance business efficiency. Outside of work, I love experimenting with new technologies, contributing to personal projects, and connecting with others who share a passion for building meaningful software.
+        </p>
+    </section>
+  )
+}
